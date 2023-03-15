44daf91b1344ba7b6c284c71f4474769cf89ab26
Ruchira (Richie) Jayasekara <me@ruchij.com>
2023-03-15 18:52:46 +1100

    Remove unused imports

--- api/src/main/java/com/ruchij/utils/JsonUtils.java
+++ api/src/main/java/com/ruchij/utils/JsonUtils.java
@@ -1,6 +1,5 @@
 package com.ruchij.utils;
 
-import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.*;
