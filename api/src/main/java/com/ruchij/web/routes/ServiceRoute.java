52149921e6046df5a61f115231bdb4de5bc9df41
Ruchira (Richie) Jayasekara <me@ruchij.com>
2025-04-28 23:12:41 +1000

    Using sync API for ServiceRoutes

--- api/src/main/java/com/ruchij/web/routes/ServiceRoute.java
+++ api/src/main/java/com/ruchij/web/routes/ServiceRoute.java
@@ -1,6 +1,7 @@
 package com.ruchij.web.routes;
 
 import com.ruchij.service.health.HealthService;
+import com.ruchij.service.health.models.ServiceInformation;
 import io.javalin.apibuilder.EndpointGroup;
 import io.javalin.http.HttpStatus;
 
@@ -10,24 +11,20 @@ import static io.javalin.apibuilder.ApiBuilder.get;
 import static io.javalin.apibuilder.ApiBuilder.path;
 
 public class ServiceRoute implements EndpointGroup {
-    private final HealthService healthService;
+  private final HealthService healthService;
 
-    public ServiceRoute(HealthService healthService) {
-        this.healthService = healthService;
-    }
+  public ServiceRoute(HealthService healthService) {
+    this.healthService = healthService;
+  }
 
-    @Override
-    public void addEndpoints() {
-        path("/info", () ->
-            get(context -> context.future(() ->
-                    CompletableFuture.supplyAsync(healthService::serviceInformation)
-                        .thenAccept(serviceInformation ->
-                            context
-                                .status(HttpStatus.OK)
-                                .json(serviceInformation)
-                        )
-                )
-            )
-        );
-    }
+  @Override
+  public void addEndpoints() {
+    path("/info", () ->
+        get(context -> {
+              ServiceInformation serviceInformation = this.healthService.serviceInformation();
+              context.status(HttpStatus.OK).json(serviceInformation);
+            }
+        )
+    );
+  }
 }
