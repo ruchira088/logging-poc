e60756fc2581b9d40f216b959c983e1f7efe56e6
Ruchira (Richie) Jayasekara <me@ruchij.com>
2025-09-07 00:07:54 +1000

    Enhance Elasticsearch security and update Docker Compose for improved integration

---
+++ elk/elasticsearch/create_users.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+curl -s -X POST \
+  "$ELASTICSEARCH_HOST/_security/user/$KIBANA_USER" \
+  -u "$ELASTICSEARCH_USERNAME:$ELASTICSEARCH_PASSWORD" \
+  -H "Content-Type: application/json" \
+  --data "{
+    \"password\" : \"$KIBANA_PASSWORD\",
+    \"roles\" : [ \"superuser\", \"kibana_admin\", \"kibana_system\" ]
+  }"
+
+curl -s -X POST \
+  "$ELASTICSEARCH_HOST/_security/user/$LOGSTASH_USER" \
+  -u "$ELASTICSEARCH_USERNAME:$ELASTICSEARCH_PASSWORD" \
+  -H "Content-Type: application/json" \
+  --data "{
+    \"password\" : \"$LOGSTASH_PASSWORD\",
+    \"roles\" : [ \"superuser\" ]
+  }"
